d99edd9e759f1c6389a17af78aba7d04023486dd
dwdwdw020308 <115512931+LeeJoNyun@users.noreply.github.com>
2025-06-19 18:16:36 +0900

    작업중 커밋

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
-const get = (target) => document.querySelector(target)
-const getAll = (target) => document.querySelectorAll(target)
-
-
+const get = (target) => document.querySelector(target);
+const getAll = (target) => document.querySelectorAll(target);
 
 const scroll = (e) => {
-    const topBanner = get('.top_banner')
-    if (window.scrollY === 0 && e.deltaY < 0) {
-        topBanner.classList.add('on')
-    } else {
-        topBanner.classList.remove('on')
-    }
-}
-
+    const topBanner = get('.top_banner');
+    window.addEventListener('wheel', (e) => {
+        if (window.scrollY === 0 && e.deltaY < 0) {
+            topBanner.classList.add('on');
+        } else {
+            topBanner.classList.remove('on');
+        }
+    });
+};
 
 const init = () => {
-
-}
+    scroll();
+};
 
 init();
